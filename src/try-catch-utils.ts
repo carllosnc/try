c5ab1bc309a4b2410b7639fab07a4f35e893a734
Carlos Costa <carllos.nc@gmail.com>
2025-03-04 11:42:43 -0300

    refactor: replace async-try-catch with try-catch-utils for improved error handling

--- src/try-catch-utils.ts
+++ src/try-catch-utils.ts
@@ -1,3 +1,29 @@
+export function tryCatch<T>(action: () => T) {
+  let currentError: Error | null = null
+  let data = null
+
+  try {
+    data = action()
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      currentError = e
+    } else {
+      currentError = new Error('Unknown error')
+    }
+  }
+
+  return {
+    data,
+    isError: currentError instanceof Error,
+    error: {
+      message: currentError?.message || null,
+      stack: currentError?.stack || null,
+      name: currentError?.name || null,
+      cause: currentError?.cause || null,
+    },
+  }
+}
+
 export async function tryCatchAsync<T>(action: () => Promise<T>) {
   let currentError: Error | null = null
   let data: T | null = null
