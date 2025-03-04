46127e972384b6f6bbd942d84668297b25e6452c
Carlos Costa <carllos.nc@gmail.com>
2025-03-04 11:42:47 -0300

    refactor: update import path for tryCatchAsync to use try-catch-utils

--- test/async-try-catch.test.ts
+++ test/async-try-catch.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'bun:test'
-import { tryCatchAsync } from '../src/async-try-catch'
+import { tryCatchAsync } from '../src/try-catch-utils'
 
 describe('tryCatchAsync function', () => {
   test('should handle successful async operations', async () => {
